cd9942e7d911fbd2c0a165ca6472e756d9096c82
redto0 <97466276+redto0@users.noreply.github.com>
2024-10-12 20:30:19 -0400

    nothing important

--- fetch_script.js
+++ fetch_script.js
@@ -1,9 +1,14 @@
-
-async function getText(file) {
-    let myObject = await fetch(file);
-    let myText = await myObject.text();
-    // myDisplay(myText);
-    document.getElementById(myText).innerHTML = 5 + 6 /*myText.toString()*/;
-} 
-
-getText("file.txt");
+async function getData() {
+    const url = "https://f2d0-141-215-206-98.ngrok-free.app/hello";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const outputDiv = document.getElementById('output');
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
